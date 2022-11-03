788afd697820df4b4001157ab1e89c975aa8e235
Christian Rivero Arnez <70019894+DwijanX@users.noreply.github.com>
2022-11-03 09:55:56 -0400

    Confirmation criteria about no numbers on certain inputs done

--- src/presenter.js
+++ src/presenter.js
@@ -5,6 +5,7 @@ const CourseName = document.querySelector("#courseName");
 const DateInit = document.querySelector("#dateInit");
 const DateFin = document.querySelector("#dateFin");
 const form=document.querySelector("#HomeworkCreation-form");
+const noNumberFields=document.querySelectorAll(".noNumberField")
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -15,3 +16,17 @@ form.addEventListener("submit", (event) => {
   const dateFin = DateFin.value;
   createHomework(hmwkName,dateInit,dateFin,courseName)
 });
+
+
+
+noNumberFields.forEach(noNumberField=>
+  {
+    noNumberField.addEventListener("input", (event) => {
+      event.preventDefault();
+      let currentLength=String(noNumberField.value).length-1
+      if (isNaN(noNumberField.value[currentLength])==false)
+      {
+        noNumberField.value = noNumberField.value.slice(0,currentLength-1)
+      }
+    });
+  })
