bb3b543a97209080923179860f703a642cd97703
Christian Rivero Arnez <70019894+DwijanX@users.noreply.github.com>
2022-11-03 09:28:39 -0400

    added user interface to create homework

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,17 @@
-import sumar from "./sumador";
+import createHomework from "./docente";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
+const HomeworkName = document.querySelector("#HomeworkName");
+const CourseName = document.querySelector("#courseName");
+const DateInit = document.querySelector("#dateInit");
+const DateFin = document.querySelector("#dateFin");
+const form=document.querySelector("#HomeworkCreation-form");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
-
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  const hmwkName = HomeworkName.value;
+  const courseName = CourseName.value;
+  const dateInit = DateInit.value;
+  const dateFin = DateFin.value;
+  createHomework(hmwkName,dateInit,dateFin,courseName)
 });
