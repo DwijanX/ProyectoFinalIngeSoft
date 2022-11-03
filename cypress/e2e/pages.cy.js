9d0e7ef70715f8214341eb848d288a235c9449ca
Nicolas <nicolascari.rz@gmail.com>
2022-11-03 09:59:07 -0400

    se puede cambiar de pagina y anadiendo cypress

--- cypress/e2e/pages.cy.js
+++ cypress/e2e/pages.cy.js
@@ -3,4 +3,8 @@ describe("cambiar pagina", () => {
     cy.visit('http://127.0.0.1:5500/docentes.html');
      cy.get("#title").should("contain", "Docentes");
     });
+    it("deberia entrar a pagina de docentes", () => {
+        cy.visit('http://127.0.0.1:5500/estudiantes.html');
+         cy.get("#title").should("contain", "Estudiantes");
+        });
 });
