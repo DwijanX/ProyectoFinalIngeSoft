cd9a5d948ff07c4cc05c66975c1a5e86345573c6
ChristianRivero <christianriveroarnez@gmail.com>
2022-11-04 19:29:50 -0400

    Fixed multi html with npm run

--- src/presenter.js
+++ src/presenter.js
@@ -1,9 +1,19 @@
+const {readFileSync, promises: fsPromises} = require('fs');
 
 const docentesPage = document.querySelector("#docentesPage");
 const estudiantesPage = document.querySelector("#estudiantesPage");
+const MainDiv = document.querySelector("#MainDiv");
+
+
 
 docentesPage.addEventListener("click", (event) => {
-  event.preventDefault();
-
-  document.location.href = "./docentes.html";
+  const contents = readFileSync("HtmlCode\\docentes.html", 'utf-8');
+  MainDiv.innerHTML=contents;
+  
 });
+estudiantesPage.addEventListener("click", (event) => {
+  const contents = readFileSync("HtmlCode\\estudiantes.html", 'utf-8');
+  MainDiv.innerHTML=contents;
+  
+});
+
