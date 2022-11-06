aa771b384f32cead56a18eb9bd5393debb142c56
santytorrico <90476969+santytorrico@users.noreply.github.com>
2022-11-06 17:58:28 -0400

    bug fixes

--- src/presenter.js
+++ src/presenter.js
@@ -13,11 +13,9 @@ const createCourseForm=document.querySelector("#CourseCreation-form");
 docentesPage.addEventListener("click", (event) => {
   event.preventDefault();
 
-  const hmwkName = HomeworkName.value;
-  const courseName = CourseName.value;
-  const dateInit = DateInit.value;
-  const dateFin = DateFin.value;
-  createHomework(hmwkName,dateInit,dateFin,courseName)
+  TeachersDiv.style.display="flex"
+  StudentsDiv.style.display="none"
+  resetMinorObjsStyles()
   
 });
 estudiantesPage.addEventListener("click", (event) => {
