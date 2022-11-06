9d9b73b26ae75377a0f6034b7ab955983dbe61bb
ChristianRivero <christianriveroarnez@gmail.com>
2022-11-06 13:38:55 -0400

    refactored courses array to a JsObject to  make manipulation easier

--- src/docente.js
+++ src/docente.js
@@ -1,56 +1,46 @@
 import Tarea from "./tarea.js"
 import Materia from "./materia.js";
-let coursesArray=[]
+let courses={}
 
 function createHomework(name,dateInit,dateFin,courseName, id)
 {
     let task=new Tarea(name,dateInit,dateFin,courseName, id);
-    let courseIndex=findCourse(courseName)
-    if(courseIndex!=-1)
+    if(courses[courseName])
     {
-        coursesArray[courseIndex].addHomework(task)
+        courses[courseName].addHomework(task)
         return task.getHomeworkObj()
     }
     return null
 }
-function findCourse(name)
-{
-    for(let index=0;index<coursesArray.length;index++)
-    {
-        if(coursesArray[index].CompareName(name))
-            return index;
-    }
-    return -1;
-}
 function getCourseHomeworks(courseName)
 {
-    let courseIndex=findCourse(courseName)
-    if(courseIndex!=-1)
-        return coursesArray[courseIndex].getHomeworkArray()
+    if(courses[courseName])
+    {
+        return courses[courseName].getHomeworkArray()
+    }
     return null
 }
 
 function getHomeworkBasedOnId(id)
 {
-    for(let CourseIndex=0;CourseIndex<coursesArray.length;CourseIndex++)
-    {
-        let CourseHomeworks=coursesArray[CourseIndex].getHomeworkArray()
+    for (const courseName in courses) {
+        let CourseHomeworks=courses[courseName].getHomeworkArray()
         for(let HomeworkIndex=0; HomeworkIndex<CourseHomeworks.length; HomeworkIndex++)
         {
             if(CourseHomeworks[HomeworkIndex].getId() == id)
                 return CourseHomeworks[HomeworkIndex]
         }
-    }
+      }
     return null
 }
 
 function clearCourseHomeworks(courseName)
 {
-    let courseIndex=findCourse(courseName)
+    
     let status=1
-    if(courseIndex!=-1)
+    if(courses[courseName])
     {
-        coursesArray[courseIndex].clearHomeworks()
+        courses[courseName].clearHomeworks()
         status=0
     }
     return status
@@ -58,10 +48,8 @@ function clearCourseHomeworks(courseName)
 function createCourse(Initials,Name,TeachersName)
 {
     let task=new Materia(Initials,Name,TeachersName);
-    coursesArray.push(task);
-    console.log(coursesArray);
+    courses[Name]=task
     return task.getCourseObj()
 }
-
 
 export {createHomework,getCourseHomeworks,clearCourseHomeworks,createCourse,getHomeworkBasedOnId}
