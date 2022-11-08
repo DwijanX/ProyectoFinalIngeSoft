ccfac825d9e79caeed2dac0236f67f0d78b84a91
Nicolas <nicolascari.rz@gmail.com>
2022-11-08 02:15:41 -0400

    adding test cases for new courses

--- src/estudiante.js
+++ src/estudiante.js
@@ -2,11 +2,32 @@ import Tarea from "./tarea.js"
 import Materia from "./materia.js";
 import {getCourseHomeworks} from "./docente.js";
 import SingletonCourses from "./SingleTonCourses.js";
-let courses=SingletonCourses.getInstance();
+
+let coursesStudent = [];
 
 function showCoursesOnConsole()
 {
     console.log(courses);
 }
 
-export{showCoursesOnConsole}
+function addCoursesToStudent(course)
+{
+    coursesStudent.push(course)
+}
+
+function showAllEnrolledCourses()
+{
+    let materias = "";
+    for (let i=0; i<coursesStudent.length; i++)
+    {
+        materias += coursesStudent[i]
+        if(i<coursesStudent.length-1)
+        {
+            materias += ", "
+        }
+    }
+    return materias
+}
+
+
+export{addCoursesToStudent, showAllEnrolledCourses}
--- src/estudiante.spec.js
+++ src/estudiante.spec.js
@@ -1,5 +1,19 @@
+//import { expect } from "chai";
+import {addCoursesToStudent, showAllEnrolledCourses} from "./estudiante.js"
+
 
 describe("", () => {
     it("", () => {
+    });
+});
+
+describe("agregar materias a estudiantes", () => {
+    it("se agrega una materia", () => {
+        addCoursesToStudent("mate")
+        expect(showAllEnrolledCourses()).toEqual("mate");
+    });
+    it("se agrega una materia", () => {
+        addCoursesToStudent("reli")
+        expect(showAllEnrolledCourses()).toEqual("mate, reli");
     });
 });
