c2a211bad0fa009cda620076be13db0f43eac5ed
ChristianRivero <christianriveroarnez@gmail.com>
2022-11-09 15:27:41 -0400

    fixing netlify deploy 3

--- src/presenterDocente.js
+++ src/presenterDocente.js
@@ -141,8 +141,8 @@ noNumberFields.forEach(noNumberField=>
 
 function checkIfDate1IsLowerThan2(date1,date2)
 {
-  let date1=new Date(date1);
-  let date2=new Date(date2);
+  date1=new Date(date1);
+  date2=new Date(date2);
   if(date1-date2<=0)
     return true
   else
