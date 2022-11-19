a743c66290fac4d9796ddeb9ed62693b3eea5d52
Nicolas <nicolascari.rz@gmail.com>
2022-11-19 11:07:37 -0400

    adding json student for each class

---
+++ src/predefinedStudents.js
@@ -0,0 +1,142 @@
+jsonTechWeb = {
+    "course": "TECNOLOGIAS WEB",
+    "students":
+    [
+        {
+        "name": "Aguilar Alcocer, Dayan",
+        "name": "Aliss Arteaga, Francisco Gabriel",
+        "name": "Balderrama Vargas, Gabriel Ichiro",
+        "name": "Bustamante Jaldin, Sergio",
+        "name": "Camargo Aramayo, Maria Belen",
+        "name": "Canapi Madrid, Cristian Said",
+        "name": "Cari Rodriguez, Nicolas",
+        "name": "Garcia Vacaflor, Jerson Alan",
+        "name": "Gonzalez Fernandez, Grover Leonardo",
+        "name": "Guardia Ramirez, Rodrigo",
+        "name": "Gutierrez Calizaya, Melina Aylem",
+        "name": "Riva Salinas, Paulo Ariel",
+        "name": "Rivero Arnez, Christian",
+        "name": "Rodriguez Alvarez, Noel Alejandro",
+        "name": "Roman Arevalo, Libia Nataly",
+        "name": "Roman Ledezma, Edwin Oswaldo"
+        }
+    ]
+}
+
+jsonIoT = {
+    "course": "INTERNET DE LAS COSAS",
+    "students":
+    [
+        {
+        "name": "Acero Rojas, Edwin Daniel",
+        "name": "Balderrama Vargas, Gabriel Ichiro",
+        "name": "Cari Rodriguez, Nicolas",
+        "name": "Diaz Crespo, Rene Dorian",
+        "name": "Escobar Herrada, Marco Fernando",
+        "name": "Fuentes Martinez, Aramis",
+        "name": "Garcia Arias, Alfred Brandon",
+        "name": "Guardia Ramirez, Rodrigo",
+        "name": "Lopez Soria, Jose Carlos",
+        "name": "Montaño Urquieta, Dieter",
+        "name": "Ortuño Prudencio, Jhonny Ricardo",
+        "name": "Rivero Arnez, Christian"
+        }
+    ]
+}
+
+jsonRedes = {
+    "course": "REDES DE COMPUTADORAS II",
+    "students":
+    [
+        {
+	    "name": "Aguilera Cordero, Isabel Consuelo",
+        "name": "Aliss Arteaga, Francisco Gabriel",
+	    "name": "Almendras Santa Cruz, Brian",
+        "name": "Balderrama Vargas, Gabriel Ichiro",
+        "name": "Camargo Aramayo, Maria Belen",
+        "name": "Cari Rodriguez, Nicolas",
+        "name": "Escobar Herrada, Marco Fernando",
+	    "name": "Flores Lopez, Alejandro Santiago",
+        "name": "Fuentes Martinez, Aramis",
+        "name": "Garcia Arias, Alfred Brandon",
+        "name": "Lopez Soria, Jose Carlos",
+        "name": "Rivero Arnez, Christian",
+	    "name": "Sanchez San Miguel, Jose Manuel",
+        "name": "Tarquino Cespedes, Adriana Valentina",
+	    "name": "Vera Loza, Rosa Noelia"
+        }
+    ]
+}
+
+jsonSisInfo = {
+    "course": "SISTEMAS DE INFORMACION III",
+    "students":
+    [
+        {
+        "name": "Balderrama Vargas, Gabriel Ichiro",
+        "name": "Cari Rodriguez, Nicolas",
+        "name": "Copa Mejia, Daho Fabio",
+        "name": "Escobar Herrada, Marco Fernando",
+        "name": "Garcia Arias, Alfred Brandon",
+        "name": "Guardia Ramirez, Rodrigo",
+        "name": "Gutierrez Calizaya, Melina Aylem",
+        "name": "Lopez Soria, Jose Carlos",
+        "name": "Mamani Perez, Sandra Jael",
+        "name": "Rivero Arnez, Christian",
+        "name": "Salazar Aramayo, Luis Felipe",
+	    "name": "Salazar Villegas, Luis Sebastian",
+	    "name": "Sanchez San Miguel, Jose Manuel",
+        "name": "Tarquino Cespedes, Adriana Valentina",
+	    "name": "Terceros Ortega, Jonatan Gerson",
+        "name": "Torrico Hinojosa, Santiago Jose",
+	    "name": "Uribe Tapia, Nicole Alejandra"
+        }
+    ]
+}
+
+jsonIngSoft = {
+    "course": "INGENIERIA DE SOFTWARE",
+    "students":
+    [
+        {
+        "name": "Aguayo Salas, Karen Pilar",
+        "name": "Asturizaga Garcia, Sebastian",
+        "name": "Balderrama Vargas, Gabriel Ichiro",
+        "name": "Cari Rodriguez, Nicolas",
+        "name": "Encinas Cabezas, Angy Alejandra",
+        "name": "Garcia Arias, Alfred Brandon",
+        "name": "Guardia Ramirez, Rodrigo",
+        "name": "Gutierrez Calizaya, Melina Aylem",
+        "name": "Lee Perez, Young Woang",
+        "name": "Rivero Arnez, Christian",
+        "name": "Rodriguez Coca, Alejandro Josue",
+	    "name": "Rojas Reque, Kevin Yamil",
+        "name": "Torrico Hinojosa, Santiago Jose"
+        }
+    ]
+}
+
+jsonAdmin = {
+    "course": "ADMINISTRACION II",
+    "students":
+    [
+        {
+        "name": "Alavi Gutierrez, Dilan Alvaro",
+        "name": "Arze Mejia, Jaime Alberto",
+        "name": "Balderrama Muñoz, Wendy Isabel",
+        "name": "Bustamante Jaldin, Sergio",
+        "name": "Calvimonte Siles, Mariana",
+        "name": "Camargo Aramayo, Maria Belen",
+        "name": "Cari Rodriguez, Nicolas",
+        "name": "Crespo Romero, Melany",
+        "name": "Gamboa Montaño, Kevin",
+        "name": "Lazarte Cabero, Diego",
+        "name": "Mencia Cespedes, Tatiana Dayana",
+        "name": "Quiroga Zambrana, Marcelo Sergio",
+        "name": "Rivero Arnez, Christian",
+        "name": "Sanchez San Miguel, Jose Manuel",
+	    "name": "Torrico Quilla, Gonzalo Nicolas",
+        "name": "Villegas Quiroga, Andres Sebastian"
+        }
+    ]
+}
