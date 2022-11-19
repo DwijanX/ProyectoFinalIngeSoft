6e8fa2cbde62b330f62358b8cbfffd92e1181066
Nicolas <nicolascari.rz@gmail.com>
2022-11-19 16:31:04 -0400

    fixing cypress test

--- cypress/e2e/estudiantes.cy.js
+++ cypress/e2e/estudiantes.cy.js
@@ -1,8 +1,8 @@
 describe("Despliegue de vistas", () => {
     it("deberia desplegar la vista de estudiantes", () => {
     cy.visit('http://localhost:1234');
-     cy.get("#loginText").type("Antezana Rojas, Israel Gilberto")
-     cy.get("#passwordText").type("ucb2022")
+     cy.get("#loginText").type("Cari Rodriguez, Nicolas")
+     cy.get("#passwordText").type("123")
      cy.get("#estudiantesPage").click()
     cy.get("#StudentsDiv").should("be.visible")
     });
@@ -10,7 +10,7 @@ describe("Despliegue de vistas", () => {
 describe("Funciones de vista estudiante", () => {
     it("deberia registrarse a una materia", () => {
     cy.visit('http://localhost:1234');
-    cy.get("#loginText").type("Antezana Rojas, Israel Gilberto")
+    cy.get("#loginText").type("Montaño Urquieta, Dieter")
      cy.get("#passwordText").type("123")
      cy.get("#estudiantesPage").click()
      cy.get("#enrollCourse").type("INTERNET DE LAS COSAS")
@@ -19,8 +19,8 @@ describe("Funciones de vista estudiante", () => {
     });
     it("deberia verse la tarea al registrarse a la materia", () => {
         cy.visit('http://localhost:1234');
-        cy.get("#loginText").type("Antezana Rojas, Israel Gilberto")
-         cy.get("#passwordText").type("123")
+        cy.get("#loginText").type("Montaño Urquieta, Dieter")
+     cy.get("#passwordText").type("123")
          cy.get("#estudiantesPage").click()
          cy.get("#enrollCourse").type("INTERNET DE LAS COSAS")
          cy.get("#BtnToEnrollCourse").click()
@@ -28,8 +28,8 @@ describe("Funciones de vista estudiante", () => {
         });
     it("deberia tener una dia de tareas de color rojo", () => {
         cy.visit('http://localhost:1234');
-        cy.get("#loginText").type("Antezana Rojas, Israel Gilberto")
-        cy.get("#passwordText").type("123")
+        cy.get("#loginText").type("Montaño Urquieta, Dieter")
+     cy.get("#passwordText").type("123")
         cy.get("#estudiantesPage").click()
         cy.get("#enrollCourse").type("INTERNET DE LAS COSAS")
         cy.get("#BtnToEnrollCourse").click()
